759f328ca29387f06b7cf389726bd221f1343d56
Maciej Pokorski <maciekpokorski@gmail.com>
2022-05-21 23:24:54 +0200

    use eslint recommendations

--- index.js
+++ index.js
@@ -2,68 +2,91 @@ import * as fs from 'fs/promises';
 import { stringify } from 'csv-stringify';
 import { parse } from 'csv-parse/sync';
 
-const main = async () => {
-  const orders_raw = await fs.readFile('orders.csv', 'binary');
-  const products_raw = await fs.readFile('products.csv', 'binary');
-  const customers_raw = await fs.readFile('customers.csv', 'binary');
-  const orders = parse(orders_raw, { columns: true });
-  const products = parse(products_raw, { columns: true });
-  const customers = parse(customers_raw, { columns: true });
-};
-
 export const generateOrderPrices = (orders, products) => {
-  const order_prices = [];
+  const orderPrices = [];
   orders.forEach((order) => {
-    let order_price = 0.0;
-    const order_products = order.products.split(' ');
-    order_products.forEach((product_id) => {
-      order_price += parseFloat(products[product_id].cost);
+    let orderPrice = 0.0;
+    const orderProducts = order.products.split(' ');
+    orderProducts.forEach((productId) => {
+      orderPrice += parseFloat(products[productId].cost);
     });
-    const order_price_obj = {
+    const orderPriceObj = {
       id: order.id,
       customer: order.customer,
-      euros: order_price,
+      euros: orderPrice,
     };
-    order_prices.push(order_price_obj);
+    orderPrices.push(orderPriceObj);
   });
 
-  return order_prices;
+  return orderPrices;
 };
 
 export const generateProductCustomers = (products, orders) => {
-  const product_customers = [];
+  const productCustomers = [];
   products.forEach((product) => {
-    {
-      const orders_for_product = orders.filter((order) => order.products.split(' ').includes(product.id));
-      orders_for_product.map((order) => {
-        const index_to_write = product_customers.findIndex((product_customers) => product_customers.id === product.id);
-        const first_customer_purchase = index_to_write === -1;
-        if (first_customer_purchase) {
-          product_customers.push({ id: product.id, customer_ids: order.customer });
-        } else if (!product_customers[index_to_write].customer_ids.includes(order.customer)) {
-          product_customers[index_to_write].customer_ids += ` ${order.customer}`;
-        }
-      });
-    }
-  });
-
-  return product_customers;
-};
-
-export const generateCustomerRanking = (order_prices, customers) => {
-  const customer_ranking = [];
-  customers.forEach((customer) => {
-    const total_euros = order_prices.filter((obj) => obj.customer == customer.id)
-      .reduce((a, b) => +a + +b.euros, 0);
-    customer_ranking.push({
-      id: customer.id,
-      firstname: customer.firstname,
-      lastname: customer.lastname,
-      total_euros,
+    const ordersForProduct = orders.filter((order) => order.products.split(' ').includes(product.id));
+    ordersForProduct.forEach((order) => {
+      const indexToWrite = productCustomers.findIndex(
+        (productCustomer) => productCustomer.id === product.id,
+      );
+      const firstCustomerPurchase = indexToWrite === -1;
+      if (firstCustomerPurchase) {
+        productCustomers.push({ id: product.id, customer_ids: order.customer });
+      } else if (!productCustomers[indexToWrite].customer_ids.includes(order.customer)) {
+        productCustomers[indexToWrite].customer_ids += ` ${order.customer}`;
+      }
     });
   });
 
-  return customer_ranking.sort((a, b) => b.total_euros - a.total_euros);
+  return productCustomers;
+};
+
+export const generateCustomerRanking = (orderPrices, customers) => {
+  const customerRanking = [];
+  customers.forEach((customer) => {
+    const totalEuros = orderPrices.filter((obj) => obj.customer === customer.id)
+      .reduce((a, b) => +a + +b.euros, 0);
+    customerRanking.push({
+      id: customer.id,
+      firstname: customer.firstname,
+      lastname: customer.lastname,
+      total_euros: totalEuros,
+    });
+  });
+
+  return customerRanking.sort((a, b) => b.total_euros - a.total_euros);
+};
+
+const saveCSV = (arrayOfObjects, outFilename, columns = null) => {
+  stringify(arrayOfObjects, { header: true, columns }, (error, output) => {
+    fs.writeFile(outFilename, output, (writeError) => {
+      if (writeError) throw writeError;
+    });
+    console.log(`${outFilename} saved`);
+  });
+};
+
+const main = async () => {
+  const ordersRaw = await fs.readFile('orders.csv', 'binary');
+  const productsRaw = await fs.readFile('products.csv', 'binary');
+  const customersRaw = await fs.readFile('customers.csv', 'binary');
+
+  const orders = parse(ordersRaw, { columns: true });
+  const products = parse(productsRaw, { columns: true });
+  const customers = parse(customersRaw, { columns: true });
+
+  const orderPrices = generateOrderPrices(orders, products);
+  const productCustomers = generateProductCustomers(products, orders);
+  const customerRanking = generateCustomerRanking(orderPrices, customers);
+
+  const orderPricesColumns = {
+    id: 'id',
+    euros: 'euros',
+  };
+
+  saveCSV(orderPrices, 'order_prices.csv', orderPricesColumns);
+  saveCSV(productCustomers, 'product_customers.csv');
+  saveCSV(customerRanking, 'customer_ranking.csv');
 };
 
 main();
