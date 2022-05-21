29078a92e95d7b8ddfcaac564fa81084eb708150
Maciej Pokorski <maciekpokorski@gmail.com>
2022-05-21 22:31:29 +0200

    all tests passes

--- index.js
+++ index.js
@@ -1,57 +1,73 @@
-import * from 'fs/promises';
+import * as fs from 'fs/promises';
 import { parse } from 'csv-parse/sync';
 
 const main = async () => {
-    const orders_raw = await fs.readFile("orders.csv", "binary");
-    const products_raw = await fs.readFile("products.csv", "binary");
-    const customers_raw = await fs.readFile("customers.csv", "binary");
-    const orders = parse(orders_raw, { columns: true });
-    const products = parse(products_raw, { columns: true });
-    const customers = parse(customers_raw, { columns: true });
+  const orders_raw = await fs.readFile('orders.csv', 'binary');
+  const products_raw = await fs.readFile('products.csv', 'binary');
+  const customers_raw = await fs.readFile('customers.csv', 'binary');
+  const orders = parse(orders_raw, { columns: true });
+  const products = parse(products_raw, { columns: true });
+  const customers = parse(customers_raw, { columns: true });
 
-    let order_prices = generateOrderPrices(orders, products)
-    let product_customers = generateProductCustomers(products, orders);
-}
+  const order_prices = generateOrderPrices(orders, products);
+  const product_customers = generateProductCustomers(products, orders);
+  const customer_ranking = generateCustomerRanking(order_prices, customers);
+  order_prices.map((obj) => delete obj.customer);
+};
 
 export const generateOrderPrices = (orders, products) => {
-    const order_prices = []
-    orders.forEach(order => {
-        let order_price = 0.0
-        let order_products = order.products.split(" ")
-        order_products.forEach(product_id => {
-            order_price += parseFloat(products[product_id].cost)
-        })
-        let order_price_obj = {
-            id: order.id,
-            customer: order.customer,
-            euros: order_price
-        }
-        order_prices.push(order_price_obj)
-    })
+  const order_prices = [];
+  orders.forEach((order) => {
+    let order_price = 0.0;
+    const order_products = order.products.split(' ');
+    order_products.forEach((product_id) => {
+      order_price += parseFloat(products[product_id].cost);
+    });
+    const order_price_obj = {
+      id: order.id,
+      customer: order.customer,
+      euros: order_price,
+    };
+    order_prices.push(order_price_obj);
+  });
 
-    return order_prices
-}
+  return order_prices;
+};
 
 export const generateProductCustomers = (products, orders) => {
-    const product_customers = []
-    products.forEach(product => {
-        {
-            const orders_for_product = orders.filter(order => order.products.split(" ").includes(product.id))
-            orders_for_product.map(order => {
-                const index_to_write = product_customers.findIndex(product_customers => product_customers.id === product.id)
-                const first_customer_purchase = index_to_write === -1;
-                if (first_customer_purchase) {
-                    product_customers.push({ id: product.id, customer_ids: new Set([order.customer]) })
-                } else {
-                    product_customers[index_to_write]["customer_ids"].add(order.customer)
-                }
-            }
-            )
+  const product_customers = [];
+  products.forEach((product) => {
+    {
+      const orders_for_product = orders.filter((order) => order.products.split(' ').includes(product.id));
+      orders_for_product.map((order) => {
+        const index_to_write = product_customers.findIndex((product_customers) => product_customers.id === product.id);
+        const first_customer_purchase = index_to_write === -1;
+        if (first_customer_purchase) {
+          product_customers.push({ id: product.id, customer_ids: new Set([order.customer]) });
+        } else {
+          product_customers[index_to_write].customer_ids.add(order.customer);
         }
-    })
+      });
+    }
+  });
 
-    return product_customers
-}
+  return product_customers;
+};
 
+export const generateCustomerRanking = (order_prices, customers) => {
+  const customer_ranking = [];
+  customers.forEach((customer) => {
+    const total_euros = order_prices.filter((obj) => obj.customer == customer.id)
+      .reduce((a, b) => +a + +b.euros, 0);
+    customer_ranking.push({
+      id: customer.id,
+      firstname: customer.firstname,
+      lastname: customer.lastname,
+      total_euros,
+    });
+  });
 
-main()
+  return customer_ranking.sort((a, b) => b.total_euros - a.total_euros);
+};
+
+main();
--- index.test.js
+++ index.test.js
@@ -1,53 +1,60 @@
-import { generateOrderPrices, generateProductCustomers } from './index'
+import { generateOrderPrices, generateProductCustomers, generateCustomerRanking } from './index';
 
-const products =
-  [
-    { id: '0', name: 'screwdriver', cost: '2.981163654411736' },
-    { id: '1', name: 'wrench', cost: '6.490396437000094' },
-    { id: '2', name: 'hammer', cost: '2.9037321212561906' },
-    { id: '3', name: 'sickle', cost: '8.90156976370351' },
-    { id: '4', name: 'candle', cost: '9.806494914226443' },
-    { id: '5', name: 'bandsaw', cost: '10.435252185512562' }
-  ]
+const products = [
+  { id: '0', name: 'screwdriver', cost: '2.981163654411736' },
+  { id: '1', name: 'wrench', cost: '6.490396437000094' },
+  { id: '2', name: 'hammer', cost: '2.9037321212561906' },
+  { id: '3', name: 'sickle', cost: '8.90156976370351' },
+  { id: '4', name: 'candle', cost: '9.806494914226443' },
+  { id: '5', name: 'bandsaw', cost: '10.435252185512562' },
+];
 test('test order product join', () => {
-  const orders = [{ id: '0', customer: '0', products: '1 0 1 0' }]
-  const expected_result = [{ id: '0', euros: 18.943120182823662 }]
+  const orders = [{ id: '0', customer: '0', products: '1 0 1 0' }];
+  const expected_result = [{ id: '0', customer: '0', euros: 18.943120182823662 }];
 
-  const order_prices = generateOrderPrices(orders, products)
+  const order_prices = generateOrderPrices(orders, products);
   expect(order_prices).toStrictEqual(expected_result);
 });
 
 test('test product_customers', () => {
   const orders = [{ id: '0', customer: '0', products: '1 0 1 0' },
-  { id: '1', customer: '1', products: '0 2 3' },
-  { id: '2', customer: '2', products: '1 4' }
-  ]
+    { id: '1', customer: '1', products: '0 2 3' },
+    { id: '2', customer: '2', products: '1 4' },
+  ];
   const expected_result = [{ id: '0', customer_ids: new Set(['0', '1']) },
-  { id: '1', customer_ids: new Set(['0', '2']) },
-  { id: '2', customer_ids: new Set(['1']) },
-  { id: '3', customer_ids: new Set(['1']) },
-  { id: '4', customer_ids: new Set(['2']) },
-  ]
-  const product_customers = generateProductCustomers(products, orders)
+    { id: '1', customer_ids: new Set(['0', '2']) },
+    { id: '2', customer_ids: new Set(['1']) },
+    { id: '3', customer_ids: new Set(['1']) },
+    { id: '4', customer_ids: new Set(['2']) },
+  ];
+  const product_customers = generateProductCustomers(products, orders);
   expect(product_customers).toStrictEqual(expected_result);
 });
 
 test('test customer_ranking', () => {
-  const order_prices =  [{ id: '0', customer: '0', euros: 18.943120182823662 },
-  { id: '1', customer: '22', euros: 61.42542135303493 },
-  { id: '2', customer: '57', euros: 23.145479220995195 },
-  { id: '3', customer: '22', euros: 34.399455367824324 },]
+  const order_prices = [{ id: '0', customer: '0', euros: 18.943120182823662 },
+    { id: '1', customer: '22', euros: 61.42542135303493 },
+    { id: '2', customer: '57', euros: 23.145479220995195 },
+    { id: '3', customer: '22', euros: 34.399455367824324 }];
 
-  customers = [
-    { id: '0', firstname: 'John', lastname: 'Maxwell'},
-    { id: '22', firstname: 'Paul', lastname: 'Lavoisier'},
-    { id: '57', firstname: 'Jane', lastname: 'Feynman'},
-  ]
+  const customers = [
+    { id: '0', firstname: 'John', lastname: 'Maxwell' },
+    { id: '22', firstname: 'Paul', lastname: 'Lavoisier' },
+    { id: '57', firstname: 'Jane', lastname: 'Feynman' },
+  ];
   const expected_result = [
-    {id: '22', firstname: 'Paul', lastname: 'Lavoisier', total_euros: 95.82487672085925},
-    { id: '57', firstname: 'Jane', lastname: 'Feynman', total_euros: 23.145479220995195},
-    { id: '0', firstname: 'John', lastname: 'Maxwell', total_euros: 18.94312018282366},
-  ]
-  const customer_ranking = generateCustomerRanking(order_prices, customers)
+    {
+      id: '22', firstname: 'Paul', lastname: 'Lavoisier', total_euros: 95.82487672085925,
+    },
+    {
+      id: '57', firstname: 'Jane', lastname: 'Feynman', total_euros: 23.145479220995195,
+    },
+    {
+      id: '0', firstname: 'John', lastname: 'Maxwell', total_euros: 18.943120182823662,
+    },
+  ];
+  const customer_ranking = generateCustomerRanking(order_prices, customers);
+  console.log(customer_ranking);
+  console.log(expected_result);
   expect(customer_ranking).toStrictEqual(expected_result);
 });
